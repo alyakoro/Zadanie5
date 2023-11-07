45c9ba02d12a81b392a008a4133c4e6dc4bf1c9a
alyak <alya.korobeynikova99@gmail.com>
2023-11-07 17:16:56 +0300

    z5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,27 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    print("Введите координаты сторон треугольника: ")
+    print("Сторона x1 и y1: ")
+    var x1 = readln().toInt()
+    var y1 = readln().toInt()
+    print("Сторона x2 и y2: ")
+    var x2 = readln().toInt()
+    var y2 = readln().toInt()
+    print("Сторона x3 и y3: ")
+    var x3 = readln().toInt()
+    var y3 = readln().toInt()
+    val r = 0
+    class treugol_crtoch {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+        var AB_x = 0
+        var AB_y = 0
+        var BC_x = 0
+        var BC_y = 0
+        var CA_x = 0
+        var CA_y = 0
+        fun main(){
+            AB_x = (x1 + x2)/2
+            AB_y = (y1 + y2)
+
+        }
+    }
 }
