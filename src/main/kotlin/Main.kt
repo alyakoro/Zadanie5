7f13c37037e423f61ae4605810c8d6b7fe2680c2
alyak <alya.korobeynikova99@gmail.com>
2023-11-07 18:30:09 +0300

    z5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+
+import java.math.RoundingMode
+import java.text.DecimalFormat
 import kotlin.math.sqrt
 
 data class Point(val x: Double, val y: Double)
@@ -12,6 +15,9 @@ class Triangle(val A: Point, val B: Point, val C: Point) {
         val cen_X = (lAB * C.x + lBC * A.x + lAC * B.x) / l_all
         val cen_Y = (lAB * C.y + lBC * A.y + lAC * B.y) / l_all
 
+        val p = (lAB + lBC + lAC)/2
+        val r = (sqrt((p - lAB)*(p - lBC)*(p - lAC)/p))
+        println("Радиус вписанной окружности: " + r)
         return Point(cen_X, cen_Y)
     }
 }
@@ -23,15 +29,14 @@ fun Point.distanceTo(other: Point): Double {
 }
 
 fun main() {
-    val A = Point(0.0, 0.0)
+    val df = DecimalFormat("#.###")
+    val A = Point(1.0, 3.0)
     val B = Point(4.0, 0.0)
-    val C = Point(2.0, 3.0)
+    val C = Point(2.0, 7.0)
 
     val triangle = Triangle(A, B, C)
     val cen = triangle.getcen()
 
-    println("Центр вписанной окружности: (${cen.x}, ${cen.y})")
+    println("Центр вписанной окружности: (${df.format(cen.x)} : ${df.format(cen.y)})")
 
-    val radius = cen.distanceTo(A)
-    println("Радиус вписанной окружности: $radius")
 }
